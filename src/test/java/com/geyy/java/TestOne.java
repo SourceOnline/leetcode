562fa6a5e6254233bfc665a60b91e5f430225a28
geyy <1803723001@qq.com>
2021-02-05 13:58:06 +0800

    first version

---
+++ src/test/java/com/geyy/java/TestOne.java
@@ -0,0 +1,15 @@
+package com.geyy.java;
+
+/**
+ * 类注释
+ *
+ * @author gyy
+ * @version 1.0
+ * @description:
+ * @date 2021/2/5 11:16
+ */
+public class TestOne {
+    public static void main(String[] args) {
+
+    }
+}
